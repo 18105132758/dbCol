7855914bbeb465bf8a8d8f93a80f35ecf8ddfa67
zhouyujun <zhouyj@smartdot.com.cn>
2020-02-28 09:36:24 +0800

    commit  code

--- src/dbcol/app/ui/consts/AppConsts.java
+++ src/dbcol/app/ui/consts/AppConsts.java
@@ -1,11 +1,23 @@
 package dbcol.app.ui.consts;
 
 public class AppConsts {
+	/**
+	 * 应用ID
+	 */
+	public static final String APP_ID = "dbCol";
 	
+	/**
+	 * 表数据 界面  ID
+	 */
 	public static final String TABLE_DATA_UI = "dbcol.app.ui.tableData";
-	
+	/**
+	 * 表列表 界面 ID
+	 */
 	public static final String TABLE_LIST_UI = "dbcol.app.ui.tableList";
 	
+	/**
+	 * 数据采集 界面 ID
+	 */
 	public static final String DB_COLLECT_UI = "dbcol.app.ui.dbCollection";
 	
 	
